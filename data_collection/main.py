ef3a9424f0dd2b21f7d42c7714e57d00fd4f4c30
keelin <kl967038@dal.ca>
2024-11-05 19:40:29 -0500

    bringing dev up to date

---
+++ data_collection/main.py
@@ -0,0 +1,18 @@
+import scraper as sc
+import pandas as pd
+
+if __name__ == "__main__":
+    driver = sc.create_new_driver()
+    links = sc.scrape_fragrance_links(driver)
+    print('Number of links:', len(links))
+
+    all_data = []
+    for link in links:
+        fragrance_data = sc.scrape_fragrance_data(link, driver)
+        all_data.append(fragrance_data)
+
+    df = pd.DataFrame(all_data)
+    df.to_csv("fragrance-data.csv")  # Save the data to CSV
+    driver.quit()
+
+    print('Dataset saved as CSV file')

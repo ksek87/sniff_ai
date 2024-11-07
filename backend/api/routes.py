0fb2dd597e6e09b29fb7273ba5e4e87743964790
keelin <kl967038@dal.ca>
2024-11-07 09:30:17 -0500

    pylinting for app, dataset

--- backend/api/routes.py
+++ backend/api/routes.py
@@ -1,10 +1,13 @@
 from flask import Blueprint, request, jsonify   
 from services.generate_fragrance import generate_fragrance_from_description
+    
+""" API routes for the application """
 
 api_blueprint = Blueprint('api', __name__)
 
 @api_blueprint.route('/generate_fragrance', methods=['POST'])
 def generate_fragrance():
+    """ Generate a fragrance based on a description """
     data = request.json
     description = data.get('description')
     
